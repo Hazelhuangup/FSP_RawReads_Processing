bc17f3ac7347426a8502a022c0ffef87782ee48f
Wu Huang <whuang@gruffalo.hpc.hutton.ac.uk>
2025-12-04 11:29:42 +0000

    kmer_stats name

--- bin/kmer_stats.sh
+++ bin/kmer_stats.sh
@@ -9,7 +9,7 @@ cd ${INPUT_DIR}
 grep 'Est_genome_size' */*kmer_freq.stats >Est_genome_size.txt
 grep 'peak' */*kmer_freq.stats >peak.txt
 grep 'nkmer' */*kmer_freq.stats >nkmer.txt
-tail -q -n 1 */*distinct_kmers.hist > distinct_kmers.txt
+for f in */*distinct_kmers.hist; do tail -n 1 "$f" | awk -v name="$f" '{print name, $2}';done > distinct_kmers.txt
 
 mkdir -p statistics
 mv *txt statistics
